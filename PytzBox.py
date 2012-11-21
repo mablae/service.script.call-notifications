952a3ef8a1e9f331c0ce460b025521f69828f330
Frank Epperlein <mail@shellshark.de>
2012-11-21 13:23:50 +0100

    init

---
+++ PytzBox.py
@@ -0,0 +1,261 @@
+
+import urllib2
+import re
+import socket
+import hashlib
+import mimetools
+import xml.sax
+
+
+class PytzBox:
+
+    __password = False
+    __host = False
+    __sid = None
+    __protocol = 'http'
+
+    __url_login_webcm    = '{protocol}://{host}/cgi-bin/webcm'
+    __url_login_fmwcfg   = '{protocol}://{host}/cgi-bin/firmwarecfg'
+    __data_sid_challenge = 'getpage=../html/login_sid.xml'
+    __data_login_legacy  = 'getpage=../html/de/menus/menu2.html&errorpage=../html/index.html&var:lang=de&var:pagename=home&var:menu=home&=&login:command/password={password}'
+    __data_login_sid     = 'login:command/response={response}&getpage=../html/login_sid.xml'
+
+    class LoginRequiredException(Exception): pass
+    class BoxUnreachableException(Exception): pass
+    class UnsupportedCharInPasswordException(Exception): pass
+    class LoginFailedException(Exception): pass
+    class RequestFailedException(Exception): pass
+
+    def __init__(self, password=False, host="fritz.box"):
+
+        socket.setdefaulttimeout(10)
+
+        self.__password = password
+        self.__host = host
+
+        if self.__isLoginRequired() and self.__password is False:
+            raise self.LoginRequiredException('no password given')
+
+
+    def __isLoginRequired(self):
+
+        try:
+            response = urllib2.urlopen(
+                self.__url_login_webcm.format(protocol=self.__protocol, host=self.__host),
+                self.__data_sid_challenge
+            ).read()
+        except socket.error, e:
+            raise self.BoxUnreachableException(str(e))
+        except IOError, e:
+            raise self.BoxUnreachableException(str(e))
+        else:
+            is_write_access_match = re.search(r".*<iswriteaccess>(\d)</iswriteaccess>.*", response, re.MULTILINE | re.IGNORECASE)
+            if is_write_access_match:
+                result = int(is_write_access_match.group(1))
+                if result == 0:
+                    return True
+
+        return False
+
+
+    def __loginSid(self):
+
+        # request challenge
+        try:
+            response = urllib2.urlopen(
+                self.__url_login_webcm.format(protocol=self.__protocol, host=self.__host),
+                self.__data_sid_challenge
+            )
+        except socket.error, e:
+            raise self.BoxUnreachableException(str(e))
+        except IOError, e:
+            raise self.BoxUnreachableException(str(e))
+        else:
+            if response.getcode() != 200:
+                raise self.LoginFailedException('unknown returncode')
+
+        # get challenge string
+        challenge_match = re.search(r".*<Challenge>([A-Za-z0-9]*)</Challenge>.*", response.read(), re.MULTILINE | re.IGNORECASE)
+        if challenge_match:
+            challenge_string = challenge_match.group(1)
+        else:
+            raise self.LoginFailedException('challenge string not found')
+
+        # Create a UTF-16LE string from challenge + '-' + password
+        try:
+            challenge_bf = ("%s-%s" % (challenge_string, str(self.__password))).decode('iso-8859-1').encode('utf-16le')
+        except UnicodeError:
+            #non ISO-8859-1 characters will except here (e.g. EUR)
+            raise self.UnsupportedCharInPasswordException()
+
+        # Calculate the MD5 hash
+        m = hashlib.md5()
+        m.update(challenge_bf)
+
+        # byte response string from challenge + '-' + md5_hex_value
+        response_bf = "%s-%s" % (challenge_string, m.hexdigest().lower())
+
+        # Answer the challenge
+        try:
+            response = urllib2.urlopen(
+                self.__url_login_webcm.format(protocol=self.__protocol, host=self.__host),
+                self.__data_login_sid.format(response=response_bf)
+            )
+        except socket.error, e:
+            raise self.BoxUnreachableException(str(e))
+        except IOError, e:
+            raise self.BoxUnreachableException(str(e))
+        else:
+            if response.getcode() != 200:
+                raise self.LoginFailedException('unknown returncode')
+
+        # search sid
+        search = re.search('<SID>(.*?)</SID>', response.read())
+        if search:
+            self.__sid = search.group(1)
+            return True
+        else:
+            print "Could not login"
+            return False
+
+
+    def __loginLegacy(self):
+
+        try:
+            response = urllib2.urlopen(
+                self.__url_login_webcm.format(protocol=self.__protocol, host=self.__host),
+                self.__data_login_legacy.format(password=self.__password)
+            )
+        except socket.error, e:
+            raise self.BoxUnreachableException(str(e))
+        except IOError, e:
+            raise self.BoxUnreachableException(str(e))
+        else:
+            response.getcode()
+            if response.getcode() == 200:
+                try:
+                    match = re.search('<p class="errorMessage">(.*?)</p>', response.read())
+                    if match:
+                        return False
+                except Exception, e:
+                    raise self.LoginFailedException(str(e))
+                else:
+                    self.__sid = False
+                    return True
+            else:
+                raise self.LoginFailedException('unknown returncode')
+
+
+    class __encodeMultipartFormdata:
+
+        content_type = None
+        body = None
+
+        def __init__(self, fields):
+            boundary = mimetools.choose_boundary()
+            end_line = '\r\n'
+            result = list()
+            for (key, value) in fields:
+                result.append('--' + boundary)
+                result.append('Content-Disposition: form-data; name="%s"' % key)
+                result.append('')
+                result.append(value)
+            result.append('--' + boundary + '--')
+            result.append('')
+            self.body = end_line.join(result)
+            self.content_type = 'multipart/form-data; boundary=%s' % boundary
+
+
+    def __analyzeFritzboxPhonebook(self, xml_phonebook):
+
+        class FbAbHandler(xml.sax.ContentHandler):
+
+            def __init__(self):
+                self.contact_name = ""
+                self.active       = None
+                self.phone_book = dict()
+
+            #noinspection PyUnusedLocal
+            def startElement(self,  name, args):
+                if name == "contact":
+                    self.contact_name =""
+                elif name == "realName" or name == "number":
+                    self.active = name
+
+            def endElement (self,  name):
+                if name == "realName" or name == "number":
+                    self.active = None
+
+            def characters(self,  content):
+                if self.active == "realName":
+                    self.contact_name = content
+                    if not self.contact_name in self.phone_book:
+                        self.phone_book[self.contact_name] = { 'numbers': [] }
+                if self.active == "number":
+                    if self.contact_name in self.phone_book:
+                        self.phone_book[self.contact_name]['numbers'].append(content)
+
+        handler = FbAbHandler()
+
+        try:
+            xml.sax.parseString(xml_phonebook, handler=handler)
+        except Exception, msg:
+            print "Fehler %s aufgetreten" % msg
+
+        return handler.phone_book
+
+
+
+
+    def sid(self):
+
+        if self.__sid is not None:
+            return self.__sid
+        else:
+            return False
+
+
+    def login(self):
+        if not self.__loginSid():
+            self.__loginLegacy()
+        return self
+
+
+    def getPhonebook(self):
+
+        if self.__sid is None:
+            raise self.LoginRequiredException()
+
+        data = self.__encodeMultipartFormdata( (
+            ('sid', self.__sid),
+            ('PhonebookId', str(0)),
+            ('PhonebookExportName', 'Telefonbuch'),
+            ('PhonebookExport', '')
+            )
+        )
+
+        request = urllib2.Request(
+            self.__url_login_fmwcfg.format(protocol=self.__protocol, host=self.__host),
+            data.body,
+            headers={'Content-Type': data.content_type}
+        )
+
+        try:
+            response = urllib2.urlopen(request, timeout=5)
+        except socket, e:
+            raise self.BoxUnreachableException(str(e))
+        except IOError, e:
+            raise self.BoxUnreachableException(str(e))
+        except Exception, e:
+            raise self.RequestFailedException(str(e))
+        else:
+            xml_phonebook =  response.read()
+
+        return self.__analyzeFritzboxPhonebook(xml_phonebook)
+
+
+
+if __name__ == '__main__':
+
+    from pprint import pprint
+    pprint( PytzBox(password='Secret', host='fritz.box').login().getPhonebook() )
