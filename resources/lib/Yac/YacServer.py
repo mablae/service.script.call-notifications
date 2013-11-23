06f6b1c292492f33be42149138172f282eda3b5b
Malte Blättermann <malte.blaettermann@googlemail.com>
2013-11-23 08:55:34 +0100

    More Cleanup in Yac module

--- resources/lib/Yac/YacServer.py
+++ resources/lib/Yac/YacServer.py
@@ -16,8 +16,6 @@ class YacProtocol(Protocol):
         """
         As soon as any data transform it to data
         """
-        print("Looks like some YAC data coming in:'%s'..." % data)
-        self.result = None
         self.buffer = (data[5:].split("~"))
         if len(self.buffer) > 1:
             self.result = Caller(self.buffer[0], self.buffer[1])
@@ -31,10 +29,8 @@ class YacProtocol(Protocol):
 
 
 class YacServerFactory(Factory):
-    fireMe = Event()
     def __init__(self, callback):
         self.callback = callback
-
 
     def buildProtocol(self, addr):
         return YacProtocol(self.callback)
@@ -44,11 +40,9 @@ class YacServer:
         self.listen_port = int(listen_port)
         self.desc = None
         self.callback = callback
-        # TODO: add check for enabled setting
 
     def start(self):
         f = YacServerFactory(self.callback)
-        # TODO: add check for enabled setting
         self.desc = (f, reactor.listenTCP(self.listen_port, f))
 
     def shutdown(self):
@@ -58,6 +52,4 @@ class YacServer:
         print("Closing Server")
         if self.desc is not None:
             self.desc[0].hangup_ok = True
-            #self.desc[0].stopTrying()
-            #self.desc[1].disconnect()
             self.desc = None
