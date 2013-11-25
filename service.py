447dc39456c4db0522e6af552298aca2a7aa112c
Malte Blättermann <malte.blaettermann@googlemail.com>
2013-11-25 03:02:39 +0100

    Better exit for reactor, still not perfect. Show notification in xbmc for first time :)

--- service.py
+++ service.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from twisted.internet import reactor
+from twisted.internet import task
 import sys
 from CallListenerClients.FritzboxClient import FritzboxClient
 from Yac.YacServer import YacServer
@@ -12,7 +13,6 @@ except ImportError, e:
     print "Could not find xbmc modules!"
     sys.exit()
 
-
 clientPool = {}
 serverPool = {}
 resolverPool = {}
@@ -20,20 +20,43 @@ resolverPool = {}
 xbmcAddon = xbmcaddon.Addon()
 xbmcDialog = xbmcgui.Dialog()
 
-def handleIncomingCall(Caller):
-    print "Iam into it:..!"
+def parseBoolString(theString):
+  return theString[0].upper()=='T'
+
+def handleIncomingCall(caller):
+    if caller.caller == "Unknown":
+        caller.caller = xbmcaddon.getLocalizedString(30602)
+    xbmcDialog.notification(xbmcAddon.getLocalizedString(30601) % caller.caller, caller.number)
+
 
 def initServices():
-    print("Init services...")
+    #xbmc.log('initServices() running...', xbmc.LOGDEBUG)
 
-    serverPool['yac'] = YacServer(10629, handleIncomingCall)
-    serverPool['yac'].start()
+    if parseBoolString(xbmcAddon.getSetting("server.yac.enabled")):
+        xbmc.log("Starting YAC Server ", xbmc.LOGDEBUG)
+        serverPool['yac'] = YacServer(int(xbmcAddon.getSetting("server.yac.listen_port")), handleIncomingCall)
+        serverPool['yac'].start()
 
-    clientPool['fritzbox'] = FritzboxClient("192.168.178.1", handleIncomingCall)
+    if parseBoolString(xbmcAddon.getSetting("client.fritzbox.enabled")):
+        #xbmc.log("Starting Fritzbox Client", xbmc.LOGDEBUG)
+        clientPool['fritzbox'] = FritzboxClient(xbmcAddon.getSetting("client.fritzbox.host"), handleIncomingCall)
+
 
 def bootServices():
     initServices()
+
+    # TODO Emulate SIGTERM inside XBMC in correct way
+    l = task.LoopingCall(shouldWeExit)
+    l.start(0.5)
+
     reactor.run(installSignalHandlers=0)
+
+
+def shouldWeExit():
+    if xbmc.abortRequested == True:
+        xbmc.log("shouldWeExit() - Indeed, we better stop reactor now...", xbmc.LOGDEBUG)
+        reactor.stop()
+        sys.exit()
 
 
 if __name__ == "__main__":
